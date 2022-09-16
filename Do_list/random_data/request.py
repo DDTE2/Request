28a130831bb25407d40ff6a24842c46ce7ea95f1
DDTE2 <sshagotskiy@list.ru>
2022-09-17 01:04:46 +1000

    Тест

--- Do_list/random_data/request.py
+++ Do_list/random_data/request.py
@@ -4,16 +4,15 @@ from mysql.connector import connect
 def table_gen(database_data, path):
     request = RG()
 
-    try:
-        with connect(host=database_data['host'],
-                     port=database_data['port'],
-                     user=database_data['user_name'],
-                     password=database_data['password'],
-                     database=database_data['db_name'],
-                     buffered=True) as connection:
-            with connection.cursor() as cursor:
-                print(request)
-                cursor.execute(request)
-                cursor.fetchone()
-    except Exception as error:
-        print(error)
+    ##try:
+    with connect(host=database_data['host'],
+                 port=database_data['port'],
+                 user=database_data['user_name'],
+                 password=database_data['password'],
+                 database=database_data['db_name'],
+                 buffered=True) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(request)
+            cursor.fetchone()
+    ##except Exception as error:
+        ##print(error)
